f64d969d99d33d0d59fe42dea1df1bd46ee98e80
BeTa_DaYz <betadayz888@gmail.com>
2021-03-13 16:43:56 +0100

    Setting up layout

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Layout from '../core/Layout';
 
-const Signin = () => <div>sign in</div>
-
+const Signin = () => (
+    <Layout title="Signin" description="Signin to Node React E-commerce App">
+        .....
+    </Layout>
+);
 export default Signin;
