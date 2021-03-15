05811490258f607577ebbba4ff45eee5fb581467
BeTa_DaYz <betadayz888@gmail.com>
2021-03-15 07:28:53 +0100

    Show and hide signup, sigin and signout

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link, withRouter} from 'react-router-dom'
-import {signout} from '../auth';
+import { signout, isAuthenticated } from '../auth';
 
 const isActive = (history, path) => {
     if(history.location.pathname === path) {
@@ -17,19 +17,25 @@ const Menu = ({history}) => (
                  <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
             </li>
 
-            <li className="nav-item">
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
                  <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Sign in</Link>
             </li>
 
             <li className="nav-item">
                  <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Sign up</Link>
             </li>
+                </Fragment>
+            )}
 
-            <li className="nav-item">
-                 <span className="nav-link" style={{cursor: 'pointer', color: '#ffffff'}} onClick={() => signout(() => {
-                     history.push('/')
-                 })}>Sign out</span>
-            </li>
+            {isAuthenticated() && (
+                <li className="nav-item">
+                <span className="nav-link" style={{cursor: 'pointer', color: '#ffffff'}} onClick={() => signout(() => {
+                    history.push('/')
+                })}>Sign out</span>
+           </li>
+            )}
         </ul>
     </div>
 )
