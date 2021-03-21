9812e668a69b4eebbcc6f6d7964a8e1b18deca4f
BeTa_DaYz <betadayz888@gmail.com>
2021-03-21 16:28:41 +0100

    Creating product for API

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -15,3 +15,19 @@ import {API} from '../config';
         console.log(err);
     });
 };
+
+
+export const createProduct = (userId, token, product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    }).then(response => {
+        return response.json()
+    }).catch(err => {
+        console.log(err);
+    });
+};
